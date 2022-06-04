efb0a987e2517dc928d700ba798a684c540340d3
saim777 <rken775@gmail.com>
2022-06-04 18:28:53 +0530

    Spring Core Project set up with xml configuration

---
+++ 01 SpringCore/src/main/java/Application.java
@@ -0,0 +1,17 @@
+import beans.Student;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Application {
+
+    public static void main(String...x){
+        ApplicationContext context=new ClassPathXmlApplicationContext("spring-config.xml");
+
+        var stud=(Student)context.getBean("student");
+
+        System.out.println(stud.firstName+" : "+stud.id);
+
+
+
+    }
+}
---
+++ 01 SpringCore/src/main/java/beans/Student.java
@@ -0,0 +1,12 @@
+package beans;
+
+public class Student {
+    public String firstName;
+    public int id;
+
+    public Student(String firstName,int id){
+       this.firstName=firstName;
+       this.id=id;
+    }
+
+}
